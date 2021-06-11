46b026ab7d8b0277fdb6a3678c669030ed308d17
Oleh Misarosh <olehmisar@gmail.com>
2021-06-12 01:39:33 +0300

    Add spinning wheel

--- client/src/pages/CasinoWheelPage.tsx
+++ client/src/pages/CasinoWheelPage.tsx
@@ -1,10 +1,11 @@
 import { Form, Formik } from 'formik'
-import { FC, useContext } from 'react'
+import { FC, useContext, useRef } from 'react'
 import { Button, Container } from 'react-bootstrap'
 import { toWei } from 'web3-utils'
 import * as yup from 'yup'
 import { AccountContext, CasinoContext } from '../App'
 import { TextField } from '../components/form-fields'
+import { Wheel, WheelRef } from '../components/Wheel'
 
 
 const casinoWheelSchema = yup.object({
@@ -15,13 +16,19 @@ export const CasinoWheelPage: FC = () => {
   const account = useContext(AccountContext)
   const casino = useContext(CasinoContext)
 
+  const prizes = [1, 2, 3, 4, 5]
+
+  const wheelRef = useRef<WheelRef>(null)
+
   return (
     <Container>
       <Formik
         initialValues={{ amount: '0' }}
         validationSchema={casinoWheelSchema}
         onSubmit={async ({ amount }, { resetForm }) => {
-          await casino.methods.spinWheel().send({ from: account, value: toWei(amount.toString()) })
+          await casino.methods.spinWheel().send({ from: account, value: toWei(amount) })
+          // TODO: calculate index in the smart contract
+          wheelRef.current?.spinToIndex(Math.floor(Math.random() * prizes.length))
           resetForm()
         }}
       >
@@ -32,6 +39,7 @@ export const CasinoWheelPage: FC = () => {
           </Form>
         )}
       </Formik>
+      <Wheel ref={wheelRef} prizes={prizes} />
     </Container>
   )
 }
